28ab00e6c4da9f19ad6d2b22c7d3216f0aa46f71
CherokeeBoose <cherokee_boose@live.com>
2019-03-29 15:07:45 -0400

    Add files via upload

--- manage_subscriptions_pt3/az_policy2.ps1
+++ manage_subscriptions_pt3/az_policy2.ps1
@@ -1,1 +1,6 @@
-
+﻿$policydefinitions = "C:\Users\chero\azurepolicyset.definitions.json"
+$policysetparameters = "C:\Users\chero\azurepolicyset.parameters.json"
+$Subscription= Get-AZSubscription
+$policyset= New-AzPolicySetDefinition -Name "skus-for-multiple-types-1" -DisplayName "Allowed SKUs for Storage Accounts and Virtual Machines" -Description "This policy allows you to speficy what skus are allowed for storage accounts and virtual machines" -PolicyDefinition $policydefinitions -Parameter $policysetparameters 
+ 
+New-AzPolicyAssignment -PolicySetDefinition $policyset -Name DemoPolicyAssignment -Scope ("/subscriptions/" + $Subscription.Id)  -LISTOFALLOWEDSKUS_1 "Standard_B1ls,Standard_B1ms"
